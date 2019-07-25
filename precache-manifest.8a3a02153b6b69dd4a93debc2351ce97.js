86f13e8e13a4e147549d7b0478d3f1481cb5a5d5
jhongertf <jhongertf@gmail.com>
2019-07-25 18:39:05 -0400

    Updates

---
+++ precache-manifest.8a3a02153b6b69dd4a93debc2351ce97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2e51d1976828560de542c9ec973169",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fb0d4a292023abe70438",
+    "url": "/clicky-game/static/css/main.6ba52618.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/clicky-game/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "fb0d4a292023abe70438",
+    "url": "/clicky-game/static/js/main.98860070.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
